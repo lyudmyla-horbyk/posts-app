7774bcb3b7f2827bb56a102975fd9208a09a2f0b
Liudmyla Liashuk <lyudmyla.horbyk@gmail.com>
2020-07-23 00:14:06 +0300

    add user info

---
+++ src/store/index.js
@@ -0,0 +1,43 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    users: [],
+    currentUserEmail: null
+  },
+  mutations: {
+    saveUsers(state, users) {
+      state.users = users;
+    },
+    userLogin(state, userId) {
+      state.currentUserEmail = userId;
+    }
+  },
+  actions: {
+    fetchUsers({ commit }) {
+      axios.get("https://jsonplaceholder.typicode.com/users").then(
+        response => {
+          commit("saveUsers", response.data);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    },
+    login({ commit }, userEmail) {
+      commit("userLogin", userEmail);
+    }
+  },
+  getters: {
+    allowedEmails(state) {
+      return state.users.map(user => user.email);
+    },
+    user(state) {
+      return state.users.find(user => user.email === state.currentUserEmail);
+    }
+  }
+});
